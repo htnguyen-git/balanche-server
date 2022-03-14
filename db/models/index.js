b57e1660ce5a2f18ec60a24e24245a0017604731
htnguyen-git <mail.htnguyen@gmail.com>
2022-03-14 20:14:44 +0700

    [config] database

---
+++ db/models/index.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const DATABASE_URL = process.env.DATABASE_URL;
+
+const sequelize = new Sequelize(DATABASE_URL, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
+
+const db = {};
+db.Sequelize = sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
+
--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const app = express();
 var corsOptions = {
   origin: "http://localhost:8081"
 };
+const db = require("./db/models/index");
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
 app.use(cors(corsOptions));
-// parse requests of content-type - application/json
 app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to balanche server application." });
 });
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
-console.log(process.env.DATABASE_URL)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
