00c7514525999517b00e0396b40c76cfe73e0859
htnguyen-git <mail.htnguyen@gmail.com>
2022-03-16 15:27:41 +0700

    [add] role controller

--- src/controller/roles.controller.js
+++ src/controller/roles.controller.js
@@ -10,41 +10,55 @@ const seed = async (req, res) => {
     res.status(200).json(data)
 };
 const getAll = async (req, res) => {
-    const queryStr = `SELECT * FROM roles WHERE "deletedAt" is NULL;`;
-    const data = await sequelize.query(queryStr, { type: QueryTypes.SELECT })
-    res.status(200).json(data)
+    try {
+        const queryStr = `SELECT * FROM roles WHERE "deletedAt" is NULL;`;
+        const data = await sequelize.query(queryStr, { type: QueryTypes.SELECT })
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({ message: "Fail when get roles" })
+    }
 
 };
 const add = async (req, res) => {
     const roleName = req.body.roleName;
-    const queryStr = 'INSERT INTO roles ("name","createdAt","updatedAt")'
-        + ' VALUES (:name, :currentDate, :currentDate)'
-        + ' RETURNING "id","name";'
-        ;
+    try {
+        const queryStr = 'INSERT INTO roles ("name","createdAt","updatedAt")'
+            + ' VALUES (:name, :currentDate, :currentDate)'
+            + ' RETURNING "id","name";'
+            ;
 
-    const [result, metadata] = await sequelize.query(queryStr,
-        {
-            replacements: { name: roleName, currentDate: getCurrentDate() },
-        }
-    )
-    res.status(200).json(result);
+        const [result, metadata] = await sequelize.query(queryStr,
+            {
+                replacements: { name: roleName, currentDate: getCurrentDate() },
+            }
+        )
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: "Fail to add new role" })
+    }
+
 };
 
-const remove = (req, res) => {
+const remove = async (req, res) => {
     const id = req.params.id;
-    const queryStr = 'UPDATE roles '
-        + 'SET "deletedAt" = :deletedAt'
-        + ' WHERE id=:id;'
-        ;
 
-    const [result, metadata] = await sequelize.query(queryStr, {
-        replacements: { deletedAt: getCurrentDate(), id: id }
-    })
-    console.log(result);
-    console.log(metadata);
+    try {
+        const queryStr = 'UPDATE roles '
+            + 'SET "deletedAt" = :deletedAt'
+            + ' WHERE id=:id;'
+            ;
+
+        const [result, metadata] = await sequelize.query(queryStr, {
+            replacements: { deletedAt: getCurrentDate(), id: id }
+        })
+        res.status(200).json({ message: "Remove role with id = " + id + " successfully" })
+    } catch (error) {
+        res.status(200).json({ message: "Fail to remove role with id = " + id })
+    }
+
 };
 
-const getCurrentDate = new Date(Date.now()).toISOString();
+const getCurrentDate = () => new Date(Date.now()).toISOString();
 
 module.exports = {
     seed,
