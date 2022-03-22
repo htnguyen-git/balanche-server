f34f69bc06e690324e6dc88cd69deac5b9f3a00b
htnguyen-git <mail.htnguyen@gmail.com>
2022-03-22 15:44:15 +0700

    [init] quest model

--- src/models/index.js
+++ src/models/index.js
@@ -7,9 +7,10 @@ db.sequelize = sequelize;
 db.rolesContext = require('./roles.model');
 db.usersContext = require('./user.model');
 db.userRolesContext = require('./userRoles.model');
+db.questContext = require('./quest.model');
 
-// db.sequelize.sync({ force: true }).then(() => {
+// db.sequelize.sync({ force: true }).then(() => {                                          
 //     console.log("Drop and re-sync db.");
 // });
-module.exports = db;
 
+module.exports = db;
