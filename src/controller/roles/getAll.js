1f8e09999ddac871b13bdd5dd85c4052c51989c5
htnguyen-git <mail.htnguyen@gmail.com>
2022-04-12 15:01:42 +0700

    [fix] get role with id and name

--- src/controller/roles/getAll.js
+++ src/controller/roles/getAll.js
@@ -2,7 +2,7 @@ const { sequelize } = require('../../models/index')
 const { GETALL_ERROR } = require('./message')
 const getall = async (req, res) => {
     try {
-        const data = executeQuery();
+        const data = await executeQuery();
         return res.status(200).json(data);
     } catch (error) {
         return res.status(500).json({ message: error.toString() || GETALL_ERROR })
@@ -13,8 +13,8 @@ const getall = async (req, res) => {
 // }
 
 const executeQuery = async () => {
-    const queryStr = `SELECT * FROM roles WHERE "deletedAt" is NULL;`;
-    const data = await sequelize.query(queryStr, { type: QueryTypes.SELECT });
+    const queryStr = `SELECT "id", "name" FROM "roles" WHERE "deletedAt" is NULL;`;
+    const [result, metadata] = await sequelize.query(queryStr);
     return result
 }
 
