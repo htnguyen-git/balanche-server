58f1743e8185859181e443400bbb7b16c877606a
htnguyen-git <mail.htnguyen@gmail.com>
2022-04-17 14:47:22 +0700

    [init] import csv

---
+++ src/controller/quest/importCSV.js
@@ -0,0 +1,18 @@
+const { sequelize } = require('../../models/index');
+const { IMPORT_ERROR } = require('./msg');
+
+const importCSV = async (req, res) => {
+
+};
+
+const getInfoFromQuery = (req) => {
+
+};
+
+const executeQuery = async ({ userId }) => {
+
+};
+
+
+
+module.exports = importCSV;
