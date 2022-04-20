ffecaf4b58bc1698304639ecded395190064c530
htnguyen-git <mail.htnguyen@gmail.com>
2022-04-20 21:31:32 +0700

    configure mail

--- src/utilities/mail.js
+++ src/utilities/mail.js
@@ -2,9 +2,14 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     service: process.env.MAIL_SERVICE,
+    secure: false,
+    port: 587,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD
+    },
+    tls: {
+        ciphers: "SSLv3"
     }
 })
 
