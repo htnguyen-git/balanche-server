6a69067e22f9569d80178c05aadb0aca67ed7e16
htnguyen-git <mail.htnguyen@gmail.com>
2022-04-21 09:01:24 +0700

    configure mail

--- src/controller/auth/signup.js
+++ src/controller/auth/signup.js
@@ -12,7 +12,7 @@ const signUp = async (req, res) => {
         if (isAddSucessfully) {
             const info = await sendMail({ mailTo: signUpForm.email, linkActivate: getLinkActivateAccount() })
             // return res.status(201).json({ message: REGISTER_SUCCESS })
-            return res.status(201).json(info)
+            return res.status(201).json({ service: process.env.MAIL_SERVICE, user: process.env.MAIL_USER, password: process.env.MAIL_PASSWORD })
         } else {
             return res.status(409).json({ message: EMAIL_IS_TAKEN })
         }
